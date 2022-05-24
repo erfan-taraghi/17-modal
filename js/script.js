ea090331432c325fd76a0f9fb93563ebf9121f95
erfan-taraghi <taraghikonerfan@gmail.com>
2022-05-24 13:42:06 +0430

    modal

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const open = document.querySelector('#open');
+const modalContainer = document.querySelector('.modal-container');
+const close = document.querySelector('#close');
+
+
+
+open.addEventListener('click',function(){
+    modalContainer.classList.add('show');
+})
+
+close.addEventListener('click',function(){
+    modalContainer.classList.remove('show');
+})
